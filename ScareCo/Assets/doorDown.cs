06717afbe3aeac0d783519380b1a54bdc8f12e82
awoo8 <awoo8@uic.edu>
2021-10-09 19:21:06 -0500

    Added door scripts
    Slides the door up and down on the button click

---
+++ ScareCo/Assets/doorDown.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorDown : MonoBehaviour
+{
+
+    public GameObject door;
+    private Vector3 doorPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        doorPosition = door.transform.position;
+        Debug.Log("Going Down: " + doorPosition);
+        if(doorPosition.y > -0.85){
+            doorPosition.y -= 0.005f;
+            door.transform.position = doorPosition;
+        }
+    }
+}
---
+++ ScareCo/Assets/doorUp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorUp : MonoBehaviour
+{
+
+    public GameObject door;
+    private Vector3 doorPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        doorPosition = door.transform.position;
+        Debug.Log("Going Up: " + doorPosition);
+        if(doorPosition.y < 1.0){
+            doorPosition.y += 0.005f;
+            door.transform.position = doorPosition;
+        }
+    }
+}
