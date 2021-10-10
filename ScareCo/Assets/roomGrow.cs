e12de0da0f185fc1a380e2a5a18c92d580519daa
awoo8 <awoo8@uic.edu>
2021-10-09 21:07:50 -0500

    Added room scaling
    Shrinks/grows claustrophobia room on button click

---
+++ ScareCo/Assets/roomGrow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roomGrow : MonoBehaviour
+{
+
+    public GameObject room;
+    private Vector3 roomScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        roomScale = room.transform.localScale;
+        if(roomScale.x < 1){
+            roomScale.x += 0.005f;
+            roomScale.y += 0.005f;
+            roomScale.z += 0.005f;
+            room.transform.localScale = roomScale;
+        }
+    }
+}
